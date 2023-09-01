6e8193bc743beb240f365dfe8a13a84a663f79f2
Quang Do <quang.do.se@gmail.com>
2023-08-31 23:24:24 -0600

    fix: TD_FUCNTION_SWITCH

--- keyboards/mode/m256wh/keymaps/quang-do-se/keymap.c
+++ keyboards/mode/m256wh/keymaps/quang-do-se/keymap.c
@@ -47,6 +47,7 @@ void td_function_switch_finished (tap_dance_state_t *state, void *user_data) {
 
     switch (td_function_switch_tap_state.state) {
     case TD_SINGLE_TAP:
+        layer_on(_FUNCTION);
         break;
     case TD_SINGLE_HOLD:
         layer_on(_FUNCTION);
@@ -67,9 +68,16 @@ void td_function_switch_finished (tap_dance_state_t *state, void *user_data) {
 }
 
 void td_function_switch_reset (tap_dance_state_t *state, void *user_data) {
-    // if the key was held down and now is released then switch off the layer
-    if (td_function_switch_tap_state.state==TD_SINGLE_HOLD) {
+
+    switch (td_function_switch_tap_state.state) {
+    case TD_SINGLE_TAP:
         layer_off(_FUNCTION);
+        break;
+    case TD_SINGLE_HOLD:  // if the key was held down and now is released then switch off the layer
+        layer_off(_FUNCTION);
+        break;
+    default:
+        break;
     }
     td_function_switch_tap_state.state = TD_NONE;
 }
@@ -77,7 +85,7 @@ void td_function_switch_reset (tap_dance_state_t *state, void *user_data) {
 
 void td_safe_reboot(tap_dance_state_t *state, void *user_data) {
     td_safe_reboot_tap_state.state = get_current_dance_state(state);
-    
+
     switch (td_safe_reboot_tap_state.state) {
     case TD_SINGLE_TAP:
         break;
@@ -196,24 +204,24 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT(
-        QK_GRAVE_ESCAPE , KC_1       , KC_2   , KC_3    , KC_4   , KC_5  , KC_6   , KC_7   , KC_8   , KC_9   , KC_0                  , KC_MINS, KC_EQL , KC_BSPC        , TD(TD_MUTE),
-        KC_TAB          , KC_Q       , KC_W   , KC_E    , KC_R   , KC_T  , KC_Y   , KC_U   , KC_I   , KC_O   , KC_P                  , KC_LBRC, KC_RBRC, KC_BSLS        , KC_VOLU        ,
-        KC_LCTL         , KC_A       , KC_S   , KC_D    , KC_F   , KC_G  , KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN               , KC_QUOT         , TD(TD_RCTL_ENT), KC_VOLD        ,
-        TD(TD_LSFT_CAPS), KC_Z       , KC_X   , KC_C    , KC_V   , KC_B  , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH               , KC_RSFT         , KC_UP          , KC_RGUI        ,
-        MO(_FUNCTION)   , KC_LGUI    , KC_LALT,                    KC_SPC,                            KC_RALT, TD(TD_FUNCTION_SWITCH),          KC_LEFT, KC_DOWN        , KC_RGHT
-    ),
+                     QK_GRAVE_ESCAPE , KC_1       , KC_2   , KC_3    , KC_4   , KC_5  , KC_6   , KC_7   , KC_8   , KC_9   , KC_0                  , KC_MINS, KC_EQL , KC_BSPC        , TD(TD_MUTE),
+                     KC_TAB          , KC_Q       , KC_W   , KC_E    , KC_R   , KC_T  , KC_Y   , KC_U   , KC_I   , KC_O   , KC_P                  , KC_LBRC, KC_RBRC, KC_BSLS        , KC_VOLU        ,
+                     KC_LCTL         , KC_A       , KC_S   , KC_D    , KC_F   , KC_G  , KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN               , KC_QUOT         , TD(TD_RCTL_ENT), KC_VOLD        ,
+                     TD(TD_LSFT_CAPS), KC_Z       , KC_X   , KC_C    , KC_V   , KC_B  , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH               , KC_RSFT         , KC_UP          , KC_RGUI        ,
+                     MO(_FUNCTION)   , KC_LGUI    , KC_LALT,                    KC_SPC,                            KC_RALT, TD(TD_FUNCTION_SWITCH),          KC_LEFT, KC_DOWN        , KC_RGHT
+                     ),
     [_FUNCTION] = LAYOUT(
-        KC_GRAVE        , KC_F1      , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  ,  KC_F10               , KC_F11 , KC_F12 , KC_DEL         , KC_HOME        ,
-        _______         , _______    , KC_UP  , _______, _______, _______, _______, _______, KC_UP  , _______, _______               , _______, _______, _______        , KC_PGUP        ,
-        _______         , KC_LEFT    , KC_DOWN, KC_RGHT, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______               , _______         , _______        , KC_PGDN        ,
-        _______         , _______    , _______, _______, _______, _______, _______, _______, _______, _______, _______               , _______         , _______        , KC_END         ,
-        _______         , _______    , _______,                   _______,                            _______, TD(TD_FUNCTION_SWITCH),          _______, _______        , _______
-    ),
+                         KC_GRAVE        , KC_F1      , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  ,  KC_F10               , KC_F11 , KC_F12 , KC_DEL         , KC_HOME        ,
+                         _______         , _______    , KC_UP  , _______, _______, _______, _______, _______, KC_UP  , _______, _______               , _______, _______, _______        , KC_PGUP        ,
+                         _______         , KC_LEFT    , KC_DOWN, KC_RGHT, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______               , _______         , _______        , KC_PGDN        ,
+                         _______         , _______    , _______, _______, _______, _______, _______, _______, _______, _______, _______               , _______         , _______        , KC_END         ,
+                         _______         , _______    , _______,                   _______,                            _______, TD(TD_FUNCTION_SWITCH),          _______, _______        , _______
+                         ),
     [_SWITCH] = LAYOUT(
-        QD_BASE         , QD_FUNCTION, _______, _______, _______, _______, _______, _______, _______, _______, _______               , _______, _______, _______        , TD(TD_BOOT)    ,
-        _______         , _______    , _______, _______, _______, _______, _______, _______, _______, _______, _______               , _______, _______, _______        , _______        ,
-        _______         , _______    , _______, _______, _______, _______, _______, _______, _______, _______, _______               , _______         , _______        , _______        ,
-        _______         , _______    , _______, _______, _______, _______, NK_TOGG, _______, _______, _______, _______               , _______         , _______        , _______        ,
-        _______         , _______    , _______,                   _______,                            _______, TD(TD_FUNCTION_SWITCH),          _______, _______        , _______
-    )
+                       QD_BASE         , QD_FUNCTION, _______, _______, _______, _______, _______, _______, _______, _______, _______               , _______, _______, _______        , TD(TD_BOOT)    ,
+                       _______         , _______    , _______, _______, _______, _______, _______, _______, _______, _______, _______               , _______, _______, _______        , _______        ,
+                       _______         , _______    , _______, _______, _______, _______, _______, _______, _______, _______, _______               , _______         , _______        , _______        ,
+                       _______         , _______    , _______, _______, _______, _______, NK_TOGG, _______, _______, _______, _______               , _______         , _______        , _______        ,
+                       _______         , _______    , _______,                   _______,                            _______, TD(TD_FUNCTION_SWITCH),          _______, _______        , _______
+                       )
 };
